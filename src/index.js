bec0cda1db3f4c68b5cb9f016aa89543a5467ef2
sanketgawaderepo <sanketgawade2010@gmail.com>
2023-04-25 14:04:12 -0700

    first commit all files added

--- src/index.js
+++ src/index.js
@@ -1,13 +1,32 @@
-import React from 'react';
+import React, { useState } from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
+// import App from './App';
 import reportWebVitals from './reportWebVitals';
+
+
+function App() {
+
+  const [status, setStatus] = useState("Not Delivered")
+  const [check, setCheck] = useState("checked")
+
+  return (
+    <div>
+      <h1>
+        Package has been delivered : {status}.
+      </h1>
+      <button onClick={() => setStatus("Delivered")}>click me</button>
+      <p>Package is {check ? "Not Availabel" : "Availabel"} in warehouse</p>
+      <input type="checkbox" value="false" onChange={() => setCheck((check => !check))}></input>
+    </div>
+  )
+
+}
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <App name="Sanket" />
   </React.StrictMode>
 );
 
